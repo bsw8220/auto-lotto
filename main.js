e8bffa430aa41f7bf5805235f7cfa3fa95a27412
bsw8220 <a49208220@gmail.com>
2022-02-08 13:08:58 +0900

    결과 내용 삽입 기능 추가

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const arr = new Array()
+const nums = new Array();
+const numList =  new Array();
 
 function makeRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -7,26 +8,40 @@ function makeRandomNumber(min, max){
 function raffle(){
     for(let i = 0; i < 6; i++){
         let random = makeRandomNumber(1, 45);
-        for(let j in arr){
-            if (random == arr[j]){
+        for(let j in nums){
+            if (random == nums[j]){
                 random = makeRandomNumber(1, 45);
             }
         }
-        arr.push(random);
+        nums.push(random);
     }
-    arr.sort(function(a,b){
+    nums.sort(function(a,b){
         return a-b;
     });
-    for(let i = 0; i < arr.length; i++){
-        $(`.raffle-num${i+1}`).val(arr[i]);
+    for(let i = 0; i < nums.length; i++){
+        $(`.raffle-num${i+1}`).val(nums[i]);
+    }
+    makeRaffleList();
+}
+
+function makeRaffleList() {
+    if($(".finalNum").length == 5){
+        alert("최대 5개의 추첨 번호를 받을 수 있습니다.");
+        return;
+    } else {
+        const finalNums = nums.join();
+        $(".result-box").append(`<p class="finalNum" style="margin-top: 40px;">${finalNums}</p>`);
     }
 }
 
 $(".raffle").click(() => {
-    arr.length = 0;
+    nums.length = 0;
     raffle();
     $(".again").css("background-color", "red").css("color","white").css("cursor", "pointer").click(() => {
-        arr.length = 0;
-        raffle();
+        for(let i = 0; i < nums.length; i++){
+            $(`.raffle-num${i+1}`).val('');
+        }
+        $(".result-box").html("");
+        $(".again").css("background-color", "#efefef").css("color","black").css("cursor", "default");
     });
 })
